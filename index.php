f7596f14a6944718f573e8eac438d2a5b124034f
Mahamad ali Kadiwal <mohdkadiwal786@gmail.com>
2023-06-06 21:30:12 +0530

    add title in index file
    add title

--- index.php
+++ index.php
@@ -4,3 +4,15 @@ echo "hii";
 echo "hello";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seller Management System</title>
+</head>
+<body>
+    
+</body>
+</html>
