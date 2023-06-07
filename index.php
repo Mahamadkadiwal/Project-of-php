25219c21b0a21b9d8de1b9174821a64c5a002394
patel zeel <patelzeel99248@gmail.com>
2023-06-07 10:52:13 +0530

    name change

--- index.php
+++ index.php
@@ -3,17 +3,21 @@ echo "hello";
 echo "hii";
 echo "hello";
 echo "jeel";
+echo "patel zeel";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seller Management System</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
