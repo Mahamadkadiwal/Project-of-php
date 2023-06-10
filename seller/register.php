dc82c7b706143aee9f987b06ba1f72c3e8591431
Mahamad ali Kadiwal <mohdkadiwal786@gmail.com>
2023-06-10 10:43:28 +0530

    button update
    submit button not work

--- seller/register.php
+++ seller/register.php
@@ -1,17 +1,19 @@
-<?php 
-    require_once '../admin/database/dbcon.php';
+<?php
+require_once '../admin/database/dbcon.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Inspire seller Registration Page </title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../admin/plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
@@ -19,134 +21,146 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="../admin/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="../admin/index2.html" class="h1"><b>Inspire</b>Seller</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Register a new seller</p>
-
-      <form action="#" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="name" placeholder="Full name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="number" class="form-control" name="mobile "placeholder="Mobile number">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-mobile"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-              <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center">
-       
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i>
-          Sign up using Google+
-        </a>
+  <div class="register-box">
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="../admin/index2.html" class="h1"><b>Inspire</b>Seller</a>
       </div>
+      <div class="card-body">
+        <p class="login-box-msg">Register a new seller</p>
 
-      <a href="login.php" class="text-center">I already have a membership</a>
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
-</div>
-<!-- /.register-box -->
+        <form action="" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" name="name" placeholder="Full name">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="number" class="form-control" name="mobile" placeholder="Mobile number">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-mobile"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" name="email" placeholder="Email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" name="password" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
 
-<!-- jQuery -->
-<script src="../admin/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../admin/dist/js/adminlte.min.js"></script>
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
+                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                <label for="agreeTerms">
+                  I agree to the <a href="">terms</a>
+                </label>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <input type="submit" value="Register" name="submit" class="btn btn-primary">
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+        <div class="social-auth-links text-center">
+
+          <a href="#" class="btn btn-block btn-danger">
+            <i class="fab fa-google-plus mr-2"></i>
+            Sign up using Google+
+          </a>
+        </div>
+
+        <a href="login.php" class="text-center">I already have a membership</a>
+      </div>
+      <!-- /.form-box -->
+    </div><!-- /.card -->
+  </div>
+  <!-- /.register-box -->
+
+  <!-- jQuery -->
+  <script src="../admin/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../admin/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
 
 <?php
-   if (isset($_POST['submit'])) {
-    
-    $name = $_POST['name'];
-    $mobile = $_POST['mobile'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mobile = $_POST['mobile'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
-    $errors = []; // Array to store validation errors
-    
-    // Validate the password
-    if (empty($password)) {
-        $errors[] = "Please enter a password.";
-    }
-    if (strlen($password) < 8) {
-        $errors[] = "Invalid password. Passwords must be at least 8 characters long.";
-    }
-    if (!preg_match("/[a-z]/", $password)) {
-        $errors[] = "Invalid password. Passwords must contain at least one lowercase letter.";
-    }
-    if (!preg_match("/[A-Z]/", $password)) {
-        $errors[] = "Invalid password. Passwords must contain at least one uppercase letter.";
-    }
-    if (!preg_match("/[0-9]/", $password)) {
-        $errors[] = "Invalid password. Passwords must contain at least one number.";
-    }
-    if (!preg_match("/[!@#$%^&*()\-_=+{};:,<.>]/", $password)) {
-        $errors[] = "Invalid password. Passwords must contain at least one special character (!@#$%^&*()\-_=+{};:,<.>).";
-    }
+  $errors = []; // Array to store validation errors
 
-    // Check if there are any validation errors
-    if (!empty($errors)) {
-        // Display the validation errors
-       
-    } else {
-        // Password is valid, insert the record into the database
-        $sql = mysqli_query($con, "INSERT INTO `seller_login`(`name`, `mobile`, `email`, `password`) VALUES ('$name','$mobile','$email','$password')");
-
-        if ($sql) {
-            header('location:res.php');
-        } else {
-            echo "Error: " . mysqli_error($con);
-        }
-    }
+// Validate the password
+if (empty($password)) {
+    $errors[] = "Please enter a password.";
+}
+if (strlen($password) < 8) {
+    $errors[] = " Passwords must be at least 8 characters long.";
+}
+if (!preg_match("/[a-z]/", $password)) {
+    $errors[] = " Passwords must contain at least one lowercase letter.";
+}
+if (!preg_match("/[A-Z]/", $password)) {
+    $errors[] = " Passwords must contain at least one uppercase letter.";
+}
+if (!preg_match("/[0-9]/", $password)) {
+    $errors[] = " Passwords must contain at least one number.";
+}
+if (!preg_match("/[!@#$%^&*()\-_=+{};:,<.>]/", $password)) {
+    $errors[] = " Passwords must contain at least one special character (!@#$%^&*()\-_=+{};:,<.>).";
 }
 
+// Check if there are any validation errors
+if (!empty($errors)) {
+    // Display the validation error
+    echo '<script>';
+    echo 'var errorMessage = ' . json_encode(implode("\n", $errors)) . ';';
+    echo 'if (errorMessage) { alert(errorMessage); }';
+    echo '</script>';
+}
+
+
+
+    
+   else {
+    
+
+    $sql = mysqli_query($con, "INSERT INTO `seller_login`(`name`, `mobile`, `email`, `password`) VALUES ('$name','$mobile','$email','$password')");
+
+    if ($sql) {
+      header('location: res.php');
+      exit(); // Terminate the script to prevent further execution
+    } else {
+      header('location: index.php');
+      echo '<script>alert("* Password must be 6 characters");</script>';
+      exit(); // Terminate the script to prevent further execution
+    }
+  }
+}
 ?>
