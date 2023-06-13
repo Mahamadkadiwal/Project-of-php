1d414efbde4647d5fb439d663455071f4f1d3753
patel zeel <patelzeel99248@gmail.com>
2023-06-13 16:20:23 +0530

    admin add new catalog
    admin page  changes and add catalog , and oreder page

---
+++ admin/catalogupload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Catalog UPload</title>
+
+    <!-- Google Font: Source Sans Pro -->
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+</body>
+
+</html>
